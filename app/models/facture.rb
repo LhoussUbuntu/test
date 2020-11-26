a9904ff88614c0342067cf3219759ba40632649a
User Test03 <user.test03@logigroup.local>
2020-11-26 16:09:20 +0100

    add select to facture's regelement

--- app/models/facture.rb
+++ app/models/facture.rb
@@ -3,4 +3,9 @@ class Facture < ApplicationRecord
     belongs_to :client
     has_many :lignefactures
 
+
+    REGELEMENT = ["Espéce", "Chèque", "Virement"]
+    def self.regelement
+        REGELEMENT.map { |t| [t, t]}
+    end
 end
