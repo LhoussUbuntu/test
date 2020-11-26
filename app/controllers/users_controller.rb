5c78203ddbf31dc80e268b903aafd71a94aa5e2c
User Test03 <user.test03@logigroup.local>
2020-11-26 11:46:26 +0100

    shohw users

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,22 @@
+class UsersController < ApplicationController
+    before_action :authenticate_user!
+    before_action :set_user, only: [:show]
+  
+    def index
+      @users = User.all
+      @test = "test"
+      return @test
+    end
+  
+    def show; end
+  
+    private
+  
+    def set_user
+      @user = User.find(params[:id])
+    end
+  
+    def users_params
+      params.require(:user).permit(:id)
+    end
+end
